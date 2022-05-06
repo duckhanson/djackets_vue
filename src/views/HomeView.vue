29165a6a72ff8aeab8776fa8ad0acccf3a8aedef
Chia-Han Lu <ducklyu0301@gmail.com>
2022-05-06 15:59:22 -0500

    add simple ui and productbox @views/homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,31 @@
 <template>
   <div class="home">
-    Home
+    <section class="hero is-medium is-dark mb-6">
+      <div class="hero-body has-text-centered">
+        <p class="title mb-6">Welcome to Djacket</p>
+        <p class="subtitle">The best jacket store online</p>
+      </div>
+    </section>
+    <div class="columns is-multiline">
+      <div class="column is-12">
+        <h2 class="is-size-2 has-text-centered">Latest products</h2>
+      </div>
+
+      <ProductBox
+        v-for="product in latestProducts"
+        v-bind:key="product.id"
+        v-bind:product="product" />
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import ProductBox from '@/components/ProductBox'
 
 export default {
   name: 'HomeView',
   components: {
+    ProductBox,
   }
-}
+};
 </script>

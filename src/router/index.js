bac3d0582a8d3aa6009ff42652bb572a70442e89
Chia-Han Lu <ducklyu0301@gmail.com>
2022-05-06 21:15:13 -0500

    add Category @router & Category

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Product from '../views/Product.vue'
+import Category from '../views/Category.vue'
 
 const routes = [
   {
@@ -22,6 +23,11 @@ const routes = [
     name: 'Product',
     component: Product
   },
+  {
+    path: '/:category_slug/',
+    name: 'Category',
+    component: Category
+  }
 ];
 
 const router = createRouter({

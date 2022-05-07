9ba9e789492091b9084a5c1745c38fc2dc68e31c
Chia-Han Lu <ducklyu0301@gmail.com>
2022-05-06 20:23:38 -0500

    separate NavBar @App

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,6 @@
 <template>
   <div id="wrapper">
-    <nav class="navbar is-dark">
-      <div class="navbar-brand">
-        <router-link to="/" class="navbar-item"
-          ><strong>SXRD</strong></router-link
-        >
-        <a
-          class="navbar-burger"
-          aria-label="menu"
-          aria-expanded="false"
-          data-target="navbar-menu"
-          @click="showMobileMenu = !showMobileMenu">
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </div>
-
-      <div
-        class="navbar-menu"
-        id="navbar-menu"
-        v-bind:class="{ 'is-active': showMobileMenu }">
-        <div class="navbar-end">
-          <router-link
-            v-for="category in categories"
-            :key="category.id"
-            :to="`/${category.url}`"
-            class="navbar-item">
-            {{ category.name }}
-          </router-link>
-
-          <div class="navbar-item">
-            <div class="buttons">
-              <template v-if="$store.state.isAuthenticated">
-                <router-link to="/my-account" class="button is-light"
-                  >My account</router-link
-                >
-              </template>
-
-              <template v-else>
-                <router-link to="/log-in" class="button is-light"
-                  >Log in</router-link
-                >
-              </template>
-
-              <router-link to="/cart" class="button is-success">
-                <span class="icon"><i class="fas fa-shopping-cart"></i></span>
-                <span>Cart ({{ cartTotalLength }})</span>
-              </router-link>
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
-    
-    <div class="is-loading-bar has-text-centered" v-bind:class="{'is-loading': $store.state.isLoading }">
-      <div class="lds-dual-ring"></div>
-    </div>
+    <NavBar/>
 
     <section>
       <router-view />
@@ -72,6 +16,7 @@
 
 <script>
 import axios from 'axios';
+import NavBar from './components/NavBar.vue'
 
 export default {
   data() {
@@ -88,6 +33,10 @@ export default {
         items: []
       },
     };
+  },
+
+  components: {
+    NavBar,
   },
 
   mounted() {
@@ -114,37 +63,4 @@ export default {
 <style lang="scss">
 @import '../node_modules/bulma';
 
-.lds-dual-ring {
-  display: inline-block;
-  width: 80px;
-  height: 80px;
-}
-.lds-dual-ring:after {
-  content: " ";
-  display: block;
-  width: 64px;
-  height: 64px;
-  margin: 8px;
-  border-radius: 50%;
-  border: 6px solid #ccc;
-  border-color: #ccc transparent #ccc transparent;
-  animation: lds-dual-ring 1.2s linear infinite;
-}
-@keyframes lds-dual-ring {
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg);
-  }
-}
-.is-loading-bar {
-  height: 0;
-  overflow: hidden;
-  -webkit-transition: all 0.3s;
-  transition: all 0.3s;
-  &.is-loading {
-    height: 80px;
-  }
-}
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,103 @@
+<template>
+  <nav class="navbar is-dark">
+    <div class="navbar-brand">
+      <router-link to="/" class="navbar-item"
+        ><strong>SXRD</strong></router-link
+      >
+      <a
+        class="navbar-burger"
+        aria-label="menu"
+        aria-expanded="false"
+        data-target="navbar-menu"
+        @click="showMobileMenu = !showMobileMenu">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div
+      class="navbar-menu"
+      id="navbar-menu"
+      v-bind:class="{ 'is-active': showMobileMenu }">
+      <div class="navbar-end">
+        <router-link
+          v-for="category in categories"
+          :key="category.id"
+          :to="`/${category.url}`"
+          class="navbar-item">
+          {{ category.name }}
+        </router-link>
+
+        <div class="navbar-item">
+          <div class="buttons">
+            <template v-if="$store.state.isAuthenticated">
+              <router-link to="/my-account" class="button is-light"
+                >My account</router-link
+              >
+            </template>
+
+            <template v-else>
+              <router-link to="/log-in" class="button is-light"
+                >Log in</router-link
+              >
+            </template>
+
+            <router-link to="/cart" class="button is-success">
+              <span class="icon"><i class="fas fa-shopping-cart"></i></span>
+              <span>Cart ({{ cartTotalLength }})</span>
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'NavBar',
+  data() {
+    return {
+      // TODO: request to backend to get categories dynamically.
+      categories: [
+        { name: '烤鴨', id: 0, url: 'ducks' },
+        { name: '配料', id: 1, url: 'sides' },
+        { name: '其他品項', id: 2, url: 'others' }
+      ],
+      showMobileMenu: false,
+      cartTotalLength: parseInt(0),
+      cart: {
+        items: []
+      }
+    };
+  },
+
+  mounted() {
+    this.cart = this.$store.state.cart;
+  },
+
+  beforeCreate() {
+    // use commit to call mutations @store
+    this.$store.commit('initalizeStore');
+  },
+
+  computed: {
+    cartTotalLength() {
+      let totalLength = 0;
+      for (let i = 0; i < this.cart.items.length; i++) {
+        totalLength += this.cart.items[i].quantity;
+      }
+      return totalLength;
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>

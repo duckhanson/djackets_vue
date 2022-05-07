1fb20c50097840532c5cbc36494affd5bb42e0ed
Chia-Han Lu <ducklyu0301@gmail.com>
2022-05-07 18:31:18 -0500

    add clearCart @store

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,4 @@
-import { createStore } from 'vuex';
+import { createStore } from 'vuex'
 
 export default createStore({
   state: {
@@ -14,9 +14,9 @@ export default createStore({
   mutations: {
     initalizeStore(state) {
       if (localStorage.getItem('cart')) {
-        state.cart = JSON.parse(localStorage.getItem('cart'));
+        state.cart = JSON.parse(localStorage.getItem('cart'))
       } else {
-        localStorage.setItem('cart', JSON.stringify(state.cart));
+        localStorage.setItem('cart', JSON.stringify(state.cart))
       }
 
       if (localStorage.getItem('token')) {
@@ -31,34 +31,40 @@ export default createStore({
     addToCart(state, item) {
       const exists = state.cart.items.filter(
         (i) => i.product.id === item.product.id
-      );
+      )
 
       if (exists.length) {
         exists[0].quantity =
-          parseInt(exists[0].quantity) + parseInt(item.quantity);
+          parseInt(exists[0].quantity) + parseInt(item.quantity)
       } else {
-        state.cart.items.push(item);
+        state.cart.items.push(item)
       }
 
-      localStorage.setItem('cart', JSON.stringify(state.cart));
+      localStorage.setItem('cart', JSON.stringify(state.cart))
     },
 
     setIsLoading(state, status) {
       state.isLoading = status
     },
-    
+
     setToken(state, token) {
       state.token = token
       state.isAuthenticated = true
     },
 
     removeToken(state) {
-      state.token = '';
-      state.isAuthenticated = false;
+      state.token = ''
+      state.isAuthenticated = false
     },
 
+    clearCart(state) {
+      state.cart = {
+        items: []
+      }
+      localStorage.setItem('cart', JSON.stringify(state.cart))
+    },
 
   },
   actions: {},
   modules: {}
-});
+})
